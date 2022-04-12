4f0bcda946e1a5034510e797a91a126c1c6a337e
itucCERF <53346437+itucCERF@users.noreply.github.com>
2022-04-12 15:56:20 +0700

    Feature/change auth use bootstrap (#1)
    * Modify Login page
    
    * Change another page auth to bootstrap

--- project/resources/views/components/auth-validation-errors.blade.php
+++ project/resources/views/components/auth-validation-errors.blade.php
@@ -2,11 +2,11 @@
 
 @if ($errors->any())
     <div {{ $attributes }}>
-        <div class="font-medium text-red-600">
+        <div class="text-danger fw-bold fs-6">
             {{ __('Whoops! Something went wrong.') }}
         </div>
 
-        <ul class="mt-3 list-disc list-inside text-sm text-red-600">
+        <ul class="mt-1 text-danger fs-6">
             @foreach ($errors->all() as $error)
                 <li>{{ $error }}</li>
             @endforeach
