bbd4256d4ec35afb2d4363727fae399b020ebecc
DESKTOP-5K2RFED\DD-TUAN <bigdout401@gmail.com>
2022-04-13 17:17:18 +0700

    change app layout + add role/permission table

--- project/resources/views/components/dropdown-link.blade.php
+++ project/resources/views/components/dropdown-link.blade.php
@@ -1,1 +1,1 @@
-<a {{ $attributes->merge(['class' => 'py-3 small text-secondary fw-bold']) }}>{{ $slot }}</a>
+<a {{ $attributes->merge(['class' => 'py-3 small text-secondary text-decoration-none']) }}>{{ $slot }}</a>
--- project/resources/views/layouts/app.blade.php
+++ project/resources/views/layouts/app.blade.php
@@ -11,25 +11,31 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
         <!-- Styles -->
-        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+        <!-- <link rel="stylesheet" href="{{ asset('css/app.css') }}"> -->
+        <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
+        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 
         <!-- Scripts -->
         <script src="{{ asset('js/app.js') }}" defer></script>
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" ></script>
+        <script src="{{ asset('js/bootstrap.min.js') }}" defer></script>
     </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
+    <body class="text-dark">
+        <div class="min-vh-100">
             @include('layouts.navigation')
 
             <!-- Page Heading -->
-            <header class="bg-white shadow">
-                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+            <header class="bg-white mb-2">
+                <div class="container">
                     {{ $header }}
                 </div>
             </header>
 
             <!-- Page Content -->
             <main>
-                {{ $slot }}
+                <div class="container">
+                    {{ $slot }}
+                </div>
             </main>
         </div>
     </body>
